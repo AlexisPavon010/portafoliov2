4d92f67f718ab8597e3bf7ababa48a738f8cd7ba
Alexispavon010 <themaster034@gmail.com>
2023-11-15 18:41:13 -0300

    [update]: add gtag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,6 +7,19 @@ export default function Document() {
         <title>Alexis Uziel Pavón (@alexispavon010) - FullStack Developer</title>
         <meta name="description" content="Portafolio de @alexispavon010 (Alexis Uziel Pavón)" />
         <link rel="icon" href="/favicon.ico" />
+        {/* Fragmento de código de Google Analytics */}
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=G-T2FBWSDSV4`}></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-T2FBWSDSV4');
+              `,
+          }}
+        />
+        {/* Fin del fragmento de código de Google Analytics */}
       </Head>
       <body>
         <Main />
